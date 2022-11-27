76e955ffa327a7c9ce79149f7c6cadbc91a8fe46
Petr Cibulka <cibulka.me@gmail.com>
2022-11-27 23:05:28 +0100

    fix: remove console.error as it triggers error

--- src/helpers/preventPurge.ts
+++ src/helpers/preventPurge.ts
@@ -2,11 +2,7 @@ import path from 'path';
 import fs from 'fs';
 
 export default function preventPurge(filepath?: string) {
-  try {
-    const file = filepath || path.join(process.cwd(), 'db', 'keep_me.yaml');
-    const data = fs.readFileSync(file, 'utf-8');
-    console.log(data);
-  } catch (e) {
-    console.error(e);
-  }
+  const file = filepath || path.join(process.cwd(), 'db', 'keep_me.yaml');
+  const data = fs.readFileSync(file, 'utf-8');
+  console.log(data);
 }
